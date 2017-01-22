afc78657b0c3f74dc70887b1360717b14bf07907
Sir Nightmare <ivantochilkin@gmail.com>
2017-01-22 17:31:23 +0300

    make  script

---
+++ site_generator.py
@@ -0,0 +1,59 @@
+import jinja2
+import json
+import os
+from markdown import markdown
+
+SITE_ROOT_FOLDER = './docs/'
+ARTICLE_ROOT_FOLDER = './articles/'
+INDEX_TEMPLATE_PATH = 'templates/index_template.html'
+ARTICLE_TEMPLATE_PATH = 'templates/article_template.html'
+
+
+def ensure_dir_exists(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+
+def open_file(file_path):
+    with open(file_path, 'r', encoding="utf-8") as opened_file:
+        return opened_file.read()
+
+
+def get_config_from_json():
+    with open("config.json", "r", encoding='utf8') as json_data_file:
+        return json.load(json_data_file)
+
+
+def convert_markdown_to_html(template, file_info, article_title, topic):
+    html = markdown(file_info, extensions=['codehilite', 'fenced_code'])
+    data = {'html': html, 'title': article_title, 'topic': topic}
+    return template.render(data)
+
+
+def save_html_file(html_text, path_to_save):
+    with open(path_to_save, "w", encoding="utf-8", errors="xmlcharrefreplace") as output_file:
+        output_file.write(html_text)
+
+
+def form_path_to_html_file(markdown_path):
+    return markdown_path.replace('.md', '.html')
+
+
+if __name__ == '__main__':
+    config = get_config_from_json()
+    articles = config['articles']
+    ensure_dir_exists(SITE_ROOT_FOLDER)
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader('./'))
+    article_template = env.get_template(ARTICLE_TEMPLATE_PATH)
+    for article in articles:
+        source = article['source']
+        file_info = open_file('{}{}'.format(ARTICLE_ROOT_FOLDER, source))
+        html_article = convert_markdown_to_html(article_template, file_info, article['title'],
+                                                article['topic'])
+        path_to_save_html = '{}{}'.format(SITE_ROOT_FOLDER, form_path_to_html_file(source))
+        ensure_dir_exists(os.path.split(path_to_save_html)[0])
+        save_html_file(html_article, path_to_save_html)
+    index_template = env.get_template(INDEX_TEMPLATE_PATH)
+    index_template.globals['form_article_path'] = form_path_to_html_file
+    result_html = index_template.render(topics=config['topics'], articles=articles)
+    save_html_file(result_html, '{}{}'.format(SITE_ROOT_FOLDER, 'index.html'))
